2a3daeca8563b8a23978a28a8238fb0622e1ff1a
comav <tsyomav@gmail.com>
2021-08-02 02:03:49 +0300

    Fixed fetch bugs

--- components/bankCard.js
+++ components/bankCard.js
@@ -2,21 +2,45 @@ import React from 'react';
 import { View, Text, Image, StyleSheet, ScrollView, ImageBackground } from 'react-native';
 
 let cardImage = require('../assets/card_hamburger.png');
+let shiza = require('../assets/shiza_logo.png');
+let procard = require('../assets/procard_logo.png');
 
 export default function BankCard(props) {
     return(
         <View style={styles.wrapper}>
             <ImageBackground source={cardImage} style={styles.card} imageStyle={{borderRadius:10}} resizeMode="stretch">
-                <Text>{props.cardNumber}</Text>
+                <Text style={styles.number}>{props.cardNumber}</Text>
+                <Image resizeMethod={'resize'} source={shiza} style={styles.network}></Image>
             </ImageBackground>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
+    wrapper: {
+        display: 'flex',
+        width: '100%',
+        alignItems: 'center',
+    },
     card: {
         resizeMode: 'stretch',
         width: '98%',
         height: 220,
+        display: 'flex',
+        justifyContent: 'flex-end',
+    },
+    number: {
+        fontSize: 20,
+        backgroundColor: '#fff',
+        width: '46%',
+        marginBottom: 20,
+        marginLeft: '30%'
+    },
+    network: {
+        width: 140,
+        height: 40,
+        marginBottom: 20,
+        marginRight: 20,
+        marginLeft: '50%'
     }
 })
--- screens/bankScreen.js
+++ screens/bankScreen.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect, useLayoutEffect } from 'react';
+import { Touchable } from 'react-native';
 import { View, Text, Image, StyleSheet, ScrollView } from 'react-native';
+
 import BankCard from '../components/bankCard';
+import Delayed from '../components/delayed';
 
 export default function BankScreen() {
+
+    const [state, setState] = useState([])
+
+    useEffect(() => {
+            try {
+                const response = fetch('http://192.168.0.104:5502/getcardinfo')
+                .then((response) => response.json())
+                .then(res => setState(res));
+            } catch (error) {
+                console.log('THERES A PROBLEM W/ GET CARD FETCH')
+                throw error;
+            }
+    }, [])
+
     return (
         <View style={styles.wrapper}>
-            <BankCard style={styles.card}></BankCard>
+            <Delayed waitBeforeShow={500}>
+                <BankCard style={styles.card} cardNumber={state.number} network={state.network}/>
+            </Delayed>
         </View>
     )
 }
