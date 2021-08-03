ac44a5b022ae5c6fba17733b3be7fe739bd7924d
comav <tsyomav@gmail.com>
2021-08-03 20:56:18 +0300

    New card designs

--- components/bankCard.js
+++ components/bankCard.js
@@ -1,16 +1,25 @@
 import React from 'react';
+import { TextPropTypes } from 'react-native';
 import { View, Text, Image, StyleSheet, ScrollView, ImageBackground } from 'react-native';
 
 let cardImage = require('../assets/card_hamburger.png');
+let cardLogo;
 let shiza = require('../assets/shiza_logo.png');
 let procard = require('../assets/procard_logo.png');
 
 export default function BankCard(props) {
+
+    if (props.network = 'ProCard') {
+        cardLogo = procard;
+    } if (props.network = 'ШИZA') {
+        cardLogo = shiza;
+    }
+
     return(
         <View style={styles.wrapper}>
             <ImageBackground source={cardImage} style={styles.card} imageStyle={{borderRadius:10}} resizeMode="stretch">
                 <Text style={styles.number}>{props.cardNumber}</Text>
-                <Image resizeMethod={'resize'} source={shiza} style={styles.network}></Image>
+                <Image resizeMethod={'resize'} source={cardLogo} style={styles.network}></Image>
             </ImageBackground>
         </View>
     )
