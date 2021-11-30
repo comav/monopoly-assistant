dcb9b916a8148dbc9cf3b1cf52a42fb06a8418e6
comav <tsyomav@gmail.com>
2021-11-30 23:01:26 +0200

    Started implementing 3D card view

--- screens/cardsTab.js
+++ screens/cardsTab.js
@@ -1,8 +1,9 @@
 import 'react-native-gesture-handler';
 import React, { useState, useContext } from 'react';
-import { View, Text, Image, StyleSheet, ScrollView } from 'react-native';
+import { View, Text, Image, StyleSheet, ScrollView, Modal, TouchableOpacity } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import CardFlip from 'react-native-card-flip';
 import Card from '../components/card';
 
 import AppContext from '../components/AppContext';
@@ -13,9 +14,19 @@ const Stack = createStackNavigator();
 
 export default function CardsScreen() {
   const globalVar = useContext(AppContext);
-
+  const [modalVisible, setModalVisible] = useState(true);
   return (
     <NavigationContainer independent={true}>
+      <Modal visible={modalVisible}>
+        <CardFlip style={styles.cardContainer} ref={(cardDetails) => this.cardDetails = cardDetails} >
+          <TouchableOpacity style={styles.cardDetails} onPress={() => this.cardDetails.flip()} >
+            <View style={styles.header}>
+              <Text style={styles.headerText}>HEADER</Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.cardDetails} onPress={() => this.cardDetails.flip()} ><Text>CD</Text></TouchableOpacity>
+        </CardFlip>
+      </Modal>
       <ScrollView contentContainerStyle={styles.cardView}>
         {cardData.cards.map(x => {
           console.log(x);
@@ -43,5 +54,38 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     marginTop: 30,
     alignSelf: 'center',
+  },
+  cardDetails: {
+    display: 'flex',
+    height: '70%',
+    width: '95%',
+    backgroundColor: 'white',
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+
+    elevation: 5,
+  },
+  cardContainer: {
+    width: '100%',
+    height: '100%',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  header: {
+    height: 50,
+    width: '100%',
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    backgroundColor: 'red',
+  },
+  headerText: {
+    fontSize: 28,
+    color: 'white',
   }
 })
